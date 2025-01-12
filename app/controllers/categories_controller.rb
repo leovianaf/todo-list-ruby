72e60d7286190179f2d77afcb501aa916a620113
Leonardo da Silva Viana Filho <leonardoviana.nave@gmail.com>
2025-01-12 13:18:24 -0300

    feat: Edit and Delete categories buttons

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,25 +36,17 @@ class CategoriesController < ApplicationController
 
   # PATCH/PUT /categories/1 or /categories/1.json
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.update(category_params)
+      redirect_to lists_path, notice: "Categoria atualizada com sucesso!"
+    else
+      render :edit, alert: "Erro ao atualizar a categoria."
     end
   end
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
-    @category.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to categories_path, status: :see_other, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @category.destroy
+    redirect_to lists_path, notice: "Categoria excluída com sucesso."
   end
 
   private
